6adbf74f07a609862b52798e94de6aadf63c35ea
replaceitem <40722305+replaceitem@users.noreply.github.com>
2024-01-14 01:46:21 +0100

    Improved request data map

--- src/main/java/net/replaceitem/scarpetwebserver/ScarpetRoute.java
+++ src/main/java/net/replaceitem/scarpetwebserver/ScarpetRoute.java
@@ -13,15 +13,21 @@ import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import net.minecraft.server.command.ServerCommandSource;
 import net.replaceitem.scarpetwebserver.script.ResponseValue;
 import spark.CustomErrorPages;
+import spark.HaltException;
 import spark.Request;
 import spark.Response;
 import spark.Route;
 
 import javax.servlet.http.HttpServletResponse;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Spliterator;
+import java.util.Spliterators;
 import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 import static carpet.CarpetServer.scriptServer;
 
@@ -40,10 +46,11 @@ public class ScarpetRoute implements Route {
     public Object handle(Request request, Response response) {
         try {
             return runCallback(request, response);
+        } catch (HaltException he) {
+            throw he;
         } catch (Exception e) {
             ScarpetWebserver.LOGGER.error("Got exception when running webserver route callback on " + request.url(), e);
-            response.status(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-            return CustomErrorPages.INTERNAL_ERROR;
+            throw webserver.getService().halt(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, CustomErrorPages.INTERNAL_ERROR);
         }
     }
 
@@ -59,23 +66,34 @@ public class ScarpetRoute implements Route {
         return executingHost.callUDF(commandSource, callback, args).getString();
     }
 
+    // See https://sparkjava.com/documentation#request
     private static MapValue encodeRequestData(Request request) {
         Map<String, Value> map = new HashMap<>();
+        map.put("attributes", ListValue.wrap(request.attributes().stream().map(StringValue::of)));
         map.put("body", StringValue.of(request.body()));
-        map.put("params", mapMap(request.params(), StringValue::of, StringValue::of));
         map.put("contentLength", NumericValue.of(request.contentLength()));
         map.put("contentType", StringValue.of(request.contentType()));
         map.put("contextPath", StringValue.of(request.contextPath()));
-        map.put("url", StringValue.of(request.url()));
-        map.put("uri", StringValue.of(request.uri()));
-        map.put("protocol", StringValue.of(request.protocol()));
-        map.put("ip", StringValue.of(request.ip()));
-        map.put("host", StringValue.of(request.host()));
-        map.put("scheme", StringValue.of(request.scheme()));
-        map.put("userAgent", StringValue.of(request.userAgent()));
         map.put("cookies", mapMap(request.cookies(), StringValue::of, StringValue::of));
-        map.put("headers", ListValue.wrap(request.headers().stream().map(StringValue::of)));
+        map.put("headers", mapMap(getHeadersAsMap(request), StringValue::of, StringValue::of));
+        map.put("host", StringValue.of(request.host()));
+        map.put("ip", StringValue.of(request.ip()));
+        map.put("params", mapMap(request.params(), StringValue::of, StringValue::of));
+        map.put("pathInfo", StringValue.of(request.pathInfo()));
+        map.put("port", NumericValue.of(request.port()));
+        map.put("protocol", StringValue.of(request.protocol()));
+        map.put("queryParams", mapMap(request.raw().getParameterMap(), StringValue::new, strings -> ListValue.wrap(Arrays.stream(strings).map(StringValue::of))));
+        map.put("requestMethod", StringValue.of(request.requestMethod()));
+        map.put("scheme", StringValue.of(request.scheme()));
+        map.put("splat", ListValue.wrap(Arrays.stream(request.splat()).map(StringValue::of)));
+        map.put("uri", StringValue.of(request.uri()));
+        map.put("url", StringValue.of(request.url()));
+        map.put("userAgent", StringValue.of(request.userAgent()));
         return mapMap(map, StringValue::of, value -> value);
+    }
+    
+    private static Map<String, String> getHeadersAsMap(Request request) {
+        return StreamSupport.stream(Spliterators.spliteratorUnknownSize(request.raw().getHeaderNames().asIterator(), Spliterator.ORDERED), false).collect(Collectors.toMap(header -> header, header -> request.raw().getHeader(header)));
     }
     
     private static <K,V> MapValue mapMap(Map<K,V> map, Function<K,Value> keyMapper, Function<V,Value> valueMapper) {
